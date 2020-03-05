03b17a33d36aef4241370742d7db9cb30d81eaf3
Patrick <ppduss@gmail.com>
2020-03-04 20:44:45 -0600

    Closing Time

--- src/components/Topstories.jsx
+++ src/components/Topstories.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './topstories.scss';
 
+// IMAGES
+import manfell from '../assets/manfell.jpeg';
+import manfell2 from '../assets/manfell2.jpeg';
+import marsorbit from '../assets/Mars_Orbit_Earth.gif';
+
+
 const topstories = () => (
   <div>
     <div className="top-stories_header">Top Stories</div>
-
+    <div>{}</div>
     <div className="top-stories_content" />
+
   </div>
 );
 export default topstories;
