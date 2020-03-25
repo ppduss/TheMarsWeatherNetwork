5bc5c48946d1dcedf93a2ab0fb1bd2e1f5ddd8d4
Patrick <ppduss@gmail.com>
2020-03-24 18:47:13 -0600

    UVindex

--- src/components/UVindex/index.jsx
+++ src/components/UVindex/index.jsx
@@ -4,25 +4,28 @@ import './UVindex.scss';
 const UVindex = () => {
   const UVcontent = () => (
     <div className="UV_main">
-      <div>UV index</div>
+      <div className="UVtitle">UV index</div>
       <div className="UVlevels">
         <div>
-          <div className="green">LOW</div>
+          <div className="color green">LOW</div>
         </div>
         <div>
-          <div className="yellow">MODERATE</div>
+          <div className="color yellow">MODERATE</div>
         </div>
         <div>
-          <div className="orange">HIGH</div>
+          <div className="color orange">HIGH</div>
         </div>
         <div>
-          <div className="red">VERY HIGH</div>
+          <div className="color red">VERY HIGH</div>
         </div>
         <div>
-          <div className="purple">EXTREME</div>
+          <div className="color purple">EXTREME</div>
         </div>
       </div>
-      <div className="extreme">SPF 100 sunscreen strongly suggested</div>
+      <div className="extreme">
+        <div>EXTREME:</div>
+        Extra protection required. SPF 100 sunscreen strongly recommended
+      </div>
     </div>
   );
 
