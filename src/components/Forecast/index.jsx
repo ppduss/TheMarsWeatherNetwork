912b288f703029319ea87317467e39cddb242010
Patrick <ppduss@gmail.com>
2020-05-05 22:04:43 -0400

    Safety

--- src/components/Forecast/index.jsx
+++ src/components/Forecast/index.jsx
@@ -19,83 +19,87 @@ const Forecast = () => {
     fetchWeather();
   }, []);
 
+  const displaySelector = () => {
+    const sevenEmptySolKeys = Array(7).fill(0);
+    console.log(weatherData);
+    const solKeysArray = (weatherData && weatherData.sol_keys) || sevenEmptySolKeys;
+    const output = solKeysArray.map((sol) => {
+      const day = ((weatherData && weatherData[sol]) || sol);
+      // generate date here?
 
-  const displayDays = () => {
-    if (weatherData) {
-      const solKeysArray = weatherData.sol_keys;
-      const output = solKeysArray.map((sol) => {
-        const day = weatherData[sol];
-        const generateDate = () => {
-          if (day.First_UTC.slice(5, 7) === '01') {
-            return ('Jan');
-          } if (day.First_UTC.slice(5, 7) === '02') {
-            return ('Feb');
-          } if (day.First_UTC.slice(5, 7) === '03') {
-            return ('Mar');
-          } if (day.First_UTC.slice(5, 7) === '04') {
-            return ('Apr');
-          } if (day.First_UTC.slice(5, 7) === '05') {
-            return ('May');
-          } if (day.First_UTC.slice(5, 7) === '06') {
-            return ('June');
-          } if (day.First_UTC.slice(5, 7) === '07') {
-            return ('July');
-          } if (day.First_UTC.slice(5, 7) === '08') {
-            return ('Aug');
-          } if (day.First_UTC.slice(5, 7) === '09') {
-            return ('Sep');
-          } if (day.First_UTC.slice(5, 7) === '10') {
-            return ('Oct');
-          } if (day.First_UTC.slice(5, 7) === '11') {
-            return ('Nov');
-          } if (day.First_UTC.slice(5, 7) === '12') {
-            return ('Dec');
-          }
-        };
-        const safety = (main, element, specifics, unit) => {
-          if (main && element && specifics) {
-            return (Math.round(specifics) + unit);
-          } return ('Unavlbl');
-        };
-        return (
-          <>
-            <div className="sol">
-              <div className="day">
-                <div>
-                  {`${generateDate()} ${day.First_UTC.slice(8, 10)}`}
-                </div>
-                <div>Sol {sol}</div>
-              </div>
-              <div className="temperature">
-                <div className="empty_space" />
-                <div>{safety(day, day.AT, day.AT.av, ' °C')}</div>
-                <div>{safety(day, day.AT, day.AT.mx, ' °C')}</div>
-                <div>{safety(day, day.AT, day.AT.mn, ' °C')}</div>
-              </div>
-              <div className="wind">
-                <div className="empty_space" />
-                <div>{safety(day, day.HWS, day.HWS.av, ' m/s')}</div>
-                <div>{safety(day, day.HWS, day.HWS.mn, ' m/s')}</div>
-                <div>{safety(day, day.HWS, day.HWS.mx, ' m/s')}</div>
-                <div>{day.WD.most_common.compass_point}</div>
-              </div>
-              <div className="pressure">
-                <div>{safety(day, day.PRE, day.PRE.av, ' pa')}</div>
-              </div>
+      // const generateDate = () => {
+      //   if (day.First_UTC.slice(5, 7) === '01') {
+      //     return ('Jan');
+      //   } if (day.First_UTC.slice(5, 7) === '02') {
+      //     return ('Feb');
+      //   } if (day.First_UTC.slice(5, 7) === '03') {
+      //     return ('Mar');
+      //   } if (day.First_UTC.slice(5, 7) === '04') {
+      //     return ('Apr');
+      //   } if (day.First_UTC.slice(5, 7) === '05') {
+      //     return ('May');
+      //   } if (day.First_UTC.slice(5, 7) === '06') {
+      //     return ('June');
+      //   } if (day.First_UTC.slice(5, 7) === '07') {
+      //     return ('July');
+      //   } if (day.First_UTC.slice(5, 7) === '08') {
+      //     return ('Aug');
+      //   } if (day.First_UTC.slice(5, 7) === '09') {
+      //     return ('Sep');
+      //   } if (day.First_UTC.slice(5, 7) === '10') {
+      //     return ('Oct');
+      //   } if (day.First_UTC.slice(5, 7) === '11') {
+      //     return ('Nov');
+      //   } if (day.First_UTC.slice(5, 7) === '12') {
+      //     return ('Dec');
+      //   }
+      // };
+      // const safety = (main, element, specifics, unit) => {
+      //   if (main && element && specifics) {
+      //     return (Math.round(specifics) + unit);
+      //   } return (false);
+      // };
+
+      const displayNumbers = (value, unit) => {
+        if (value) {
+          return (Math.round(value) + unit);
+        } if (value) 
+        return 'Loading';
+      };
+      const displayDays = () => (
+        <>
+          <div className="sol">
+            <div className="day">
+              <div>{`${generateDate()} ${day.First_UTC.slice(8, 10)}` || 'Loading'}</div>
+              <div>Sol {sol}</div>
             </div>
-          </>
-        );
-      });
-
-      return output;
-    }
+            <div className="temperature">
+              <div className="empty_space" />
+              <div>{displayNumbers(day?.AT?.av, ' °C')}</div>
+              <div>{displayNumbers(day && day.AT && day.AT.mx, ' °C')}</div>
+              <div>{displayNumbers(day && day.AT && day.AT.mn, ' °C')}</div>
+            </div>
+            <div className="wind">
+              <div className="empty_space" />
+              <div>{displayNumbers(day && day.HWS && day.HWS.av, ' m/s')}</div>
+              <div>{displayNumbers(day && day.HWS.mn, ' m/s')}</div>
+              <div>{displayNumbers(day && day.HWS.mx, ' m/s')}</div>
+              <div>{day && day.WD && day.WD.most_common && day.WD.most_common.compass_point || 'Loading'}</div>
+            </div>
+            <div className="pressure">
+              <div>{displayNumbers(day && day.PRE && day.PRE.av, ' pa')}</div>
+            </div>
+          </div>
+        </>
+      ); return displayDays();
+    }); return output;
   };
 
   const ForecastContent = () => (
     <div className="sol_container">
       <div className="sol descriptors">
         <div className="day">
-          <div>Earth date</div>
+          <div>Earthian date</div>
           <div>Martian date</div>
         </div>
         <div className="temperature">
@@ -115,10 +119,10 @@ const Forecast = () => {
           <div>Avg. Press. </div>
         </div>
       </div>
-      {displayDays()}
+      {displaySelector()}
     </div>
-
   );
+
 
   return (
     <ForecastContent />
--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 // FONTS //
 import './fonts/Chicago-Regular.woff';
-// @import '../src/fonts/COMP9801-qMy5.ttf';
-// @import '../src/fonts/COMPNineteenNinetySeven.ttf';
-// @import '../src/fonts/COMPPerfectDOSVGA.ttf';
-// @import '../src/fonts/COMPSegment14.otf';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
