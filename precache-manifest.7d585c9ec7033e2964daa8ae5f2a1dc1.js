bf35bbf2eb98dd8c933f94365776e7db11d15e47
Patrick <ppduss@gmail.com>
2020-06-12 14:55:37 -0400

    Updates

---
+++ precache-manifest.7d585c9ec7033e2964daa8ae5f2a1dc1.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d0dd526ee58a30de51530f73beb16a",
+    "url": "/TheMarsWeatherNetwork/index.html"
+  },
+  {
+    "revision": "5b9b5f061c177dade575",
+    "url": "/TheMarsWeatherNetwork/static/css/2.9b46dffb.chunk.css"
+  },
+  {
+    "revision": "2169124628f196f55935",
+    "url": "/TheMarsWeatherNetwork/static/css/main.e1264f69.chunk.css"
+  },
+  {
+    "revision": "5b9b5f061c177dade575",
+    "url": "/TheMarsWeatherNetwork/static/js/2.95d3f7da.chunk.js"
+  },
+  {
+    "revision": "61fd57b06491dd87d78b2f0bbcc366a5",
+    "url": "/TheMarsWeatherNetwork/static/js/2.95d3f7da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2169124628f196f55935",
+    "url": "/TheMarsWeatherNetwork/static/js/main.bee9df18.chunk.js"
+  },
+  {
+    "revision": "6562b07aff46bad3c9f3",
+    "url": "/TheMarsWeatherNetwork/static/js/runtime-main.e6ff0396.js"
+  },
+  {
+    "revision": "9a626e7f888473fbf8cc6aba4b121d50",
+    "url": "/TheMarsWeatherNetwork/static/media/aliens.9a626e7f.jpeg"
+  },
+  {
+    "revision": "5757604044c565c57b674cb2e38e78e1",
+    "url": "/TheMarsWeatherNetwork/static/media/android2.57576040.jpeg"
+  },
+  {
+    "revision": "0855930de93c4a53d2a38b4d39bd2f24",
+    "url": "/TheMarsWeatherNetwork/static/media/atmosphere.0855930d.jpeg"
+  },
+  {
+    "revision": "594c9c661a5341d26291d73c9f47b20b",
+    "url": "/TheMarsWeatherNetwork/static/media/flat.594c9c66.jpeg"
+  },
+  {
+    "revision": "91118f3a7e470f2eb0b862fbb1053fef",
+    "url": "/TheMarsWeatherNetwork/static/media/impactcrater.91118f3a.jpeg"
+  },
+  {
+    "revision": "c1f2ca643662bb3c1678173f146f158f",
+    "url": "/TheMarsWeatherNetwork/static/media/korolevcrater.c1f2ca64.jpeg"
+  },
+  {
+    "revision": "a9d7d8050ecd50adda2b08fae7840c29",
+    "url": "/TheMarsWeatherNetwork/static/media/manFell2.a9d7d805.jpeg"
+  },
+  {
+    "revision": "2c65ccb6a20a4d0d2b61f1b42686f2a8",
+    "url": "/TheMarsWeatherNetwork/static/media/marsYellow.2c65ccb6.jpeg"
+  },
+  {
+    "revision": "ac45ebb686c28b20a056b79db528ea2e",
+    "url": "/TheMarsWeatherNetwork/static/media/marstrue.ac45ebb6.jpeg"
+  },
+  {
+    "revision": "24e320b9b89da2667838deb4e0f57156",
+    "url": "/TheMarsWeatherNetwork/static/media/mountain.24e320b9.png"
+  },
+  {
+    "revision": "8862e6fe78338ccfb83385fbb0e57cff",
+    "url": "/TheMarsWeatherNetwork/static/media/near.8862e6fe.jpeg"
+  },
+  {
+    "revision": "c062231e7c720a7674ac7b293d75d57e",
+    "url": "/TheMarsWeatherNetwork/static/media/orbit.c062231e.gif"
+  },
+  {
+    "revision": "336ff89eaa47e0d31bca0dc0a2506ec0",
+    "url": "/TheMarsWeatherNetwork/static/media/pkdBook.336ff89e.jpeg"
+  },
+  {
+    "revision": "3e2250c83268aa9ccb4ae034575b4676",
+    "url": "/TheMarsWeatherNetwork/static/media/rover.3e2250c8.jpeg"
+  },
+  {
+    "revision": "38c0205fe5b1e400d860377da22f4e52",
+    "url": "/TheMarsWeatherNetwork/static/media/rover2.38c0205f.jpeg"
+  },
+  {
+    "revision": "cddf7458625269d41d9fee596aa80a45",
+    "url": "/TheMarsWeatherNetwork/static/media/sauvage.cddf7458.jpeg"
+  },
+  {
+    "revision": "71e1907e73a6757303427c43a65cef1c",
+    "url": "/TheMarsWeatherNetwork/static/media/suburb.71e1907e.jpeg"
+  },
+  {
+    "revision": "61683ee41c51714a5ffc9bd6698496ec",
+    "url": "/TheMarsWeatherNetwork/static/media/surfaceGrey.61683ee4.jpeg"
+  },
+  {
+    "revision": "86ae87e93a903982d4dee09115889183",
+    "url": "/TheMarsWeatherNetwork/static/media/thicc.86ae87e9.jpeg"
+  }
+]);
